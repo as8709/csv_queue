b911fb19aa8c8510c21a54f8bd738f47f67200d9
as8709 <as8709@gmail.com>
2018-06-17 22:50:10 +0100

    simple html to send an ajax request with the csv file

--- web/app.py
+++ web/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 import os
+import csv_queue
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return app.send_static_file("index.html")
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    return json.dumps({'success':True})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
