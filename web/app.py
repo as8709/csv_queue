8c308f2507016e3e09a80b3b14e83003de99454b
as8709 <as8709@gmail.com>
2018-06-19 21:33:51 +0100

    slightly better handling of when the user doesn't select a file

--- web/app.py
+++ web/app.py
@@ -14,17 +14,19 @@ def root():
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    file = request.files["file"]
-    file_data = io.StringIO(file.stream.read().decode("utf-8"))
+    if "file" in request.files:
+        file = request.files["file"]
+        file_data = io.StringIO(file.stream.read().decode("utf-8"))
 
-    r = csv_queue.CsvReader(file_data,
-        os.getenv("QUEUE_NAME"),
-        os.getenv("QUEUE_IP")
-    )
-    r.send()
+        r = csv_queue.CsvReader(file_data,
+            os.getenv("QUEUE_NAME"),
+            os.getenv("QUEUE_IP")
+        )
+        r.send()
 
-
-    return "file read ok"
+        return "file read ok"
+    else:
+        return "file read not ok"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
